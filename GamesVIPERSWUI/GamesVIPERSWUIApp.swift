5bd6b74a82bad3103812031a492812da2946c3a5
Jose Bustos <35240264+korbek99@users.noreply.github.com>
2026-02-04 18:56:07 -0300

    Initial Commit

---
+++ GamesVIPERSWUI/GamesVIPERSWUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GamesVIPERSWUIApp.swift
+//  GamesVIPERSWUI
+//
+//  Created by Jose Preatorian on 04-02-26.
+//
+
+import SwiftUI
+
+@main
+struct GamesVIPERSWUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
